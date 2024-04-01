77ea2a5df6e9a9024d4ce3745f5ee243401ad0b7
Alex Bouma <alex@bouma.me>
2024-04-01 17:52:57 +0200

    Update boilerplate

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -19,6 +19,7 @@ $config
         'short_scalar_cast'                                => true,
         'phpdoc_to_comment'                                => false,
         'single_line_throw'                                => false,
+        'single_line_empty_body'                           => true,
         'single_blank_line_at_eof'                         => true,
         'no_superfluous_phpdoc_tags'                       => false,
         'linebreak_after_opening_tag'                      => true,
@@ -28,34 +29,51 @@ $config
         'single_trait_insert_per_statement'                => false,
         'nullable_type_declaration_for_default_null_value' => true,
 
-        'concat_space'                => [
+        'concat_space'                 => [
             'spacing' => 'one',
         ],
-        'array_syntax'                => [
+        'array_syntax'                 => [
             'syntax' => 'short',
         ],
-        'ordered_imports'             => [
+        'ordered_types'                => [
+            'sort_algorithm'  => 'none',
+            'null_adjustment' => 'always_last',
+        ],
+        'ordered_imports'              => [
             'imports_order'  => ['class', 'const', 'function'],
             'sort_algorithm' => 'length',
         ],
-        'cast_spaces'                 => [
+        'cast_spaces'                  => [
             'space' => 'none',
         ],
-        'curly_braces_position'       => [
+        'new_with_parentheses'         => [
+            'named_class'     => false,
+            'anonymous_class' => false,
+        ],
+        'curly_braces_position'        => [
             'anonymous_classes_opening_brace' => 'next_line_unless_newline_at_signature_end',
         ],
-        'align_multiline_comment'     => [
+        'align_multiline_comment'      => [
             'comment_type' => 'phpdocs_like',
         ],
-        'global_namespace_import'     => [
+        'global_namespace_import'      => [
             'import_classes'   => null,
             'import_constants' => null,
             'import_functions' => true,
         ],
-        'trailing_comma_in_multiline' => [
+        'type_declaration_spaces'      => [
+            'elements' => [],
+        ],
+        'nullable_type_declaration'    => [
+            'syntax' => 'question_mark',
+        ],
+        'attribute_empty_parentheses'  => [
+            'use_parentheses' => false,
+        ],
+        'trailing_comma_in_multiline'  => [
             'elements' => ['arrays', 'arguments', 'parameters', 'match'],
         ],
-        'phpdoc_align'                => [
+        'phpdoc_align'                 => [
             'align' => 'vertical',
             'tags'  => [
                 'param',
@@ -69,27 +87,30 @@ $config
                 'method',
             ],
         ],
-        'increment_style'             => [
+        'increment_style'              => [
             'style' => 'post',
         ],
-        'phpdoc_no_alias_tag'         => [
+        'phpdoc_no_alias_tag'          => [
             'replacements' => [
                 'type' => 'var',
                 'link' => 'see',
             ],
         ],
-        'no_extra_blank_lines'        => [
+        'no_extra_blank_lines'         => [
             'tokens' => [],
         ],
-        'function_declaration'        => [
+        'function_declaration'         => [
             'closure_function_spacing' => 'one',
         ],
-        'binary_operator_spaces'      => [
+        'binary_operator_spaces'       => [
             'operators' => [
                 '|'  => null,
                 '='  => 'align_single_space',
                 '=>' => 'align_single_space',
             ],
+        ],
+        'fully_qualified_strict_types' => [
+            'phpdoc_tags' => [],
         ],
     ])
     ->setFinder($finder);
